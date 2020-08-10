53eacc15ff7d08b05b2760b061ba3e6e453eb9b5
Zach Watts <zacharymwatts@gmail.com>
2020-08-09 22:59:01 -0700

    BoilerPlate for App

--- src/App.js
+++ src/App.js
@@ -1,26 +1,7 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import ReactDom from 'react-dom';
+import 'normalize.css/normalize.css';
+import './styles/styles.scss';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+ReactDom.render(<p>This is my boilerplate for my skyrim app!</p>, document.getElementById('app'));
