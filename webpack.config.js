73fd6c5b7ccbe976fda221a64c94fe0c84468885
Zach Watts <zacharymwatts@gmail.com>
2020-10-10 11:38:58 -0700

    Production Environment added

--- webpack.config.js
+++ webpack.config.js
@@ -25,7 +25,8 @@ module.exports = (env) => {
         },
         devtool: isProduction ? 'source-map' : 'cheap-module-eval-source-map',
         devServer: {
-            contentBase: path.join(__dirname, 'public')
+            contentBase: path.join(__dirname, 'public'),
+            historyApiFallback: true
         }
     };
 };
