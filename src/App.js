4eea02c3e386a448e0cec30a040a9a1fd40ce55a
Zach Watts <zacharymwatts@gmail.com>
2021-12-13 20:23:18 -0600

    Added React App into overhaul

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
-import React from 'react';
-import ReactDom from 'react-dom';
-import 'normalize.css/normalize.css';
-import './styles/styles.scss';
-import MasterForm from '../src/components/MasterForm';
+import logo from './logo.svg';
+import './App.css';
 
-ReactDom.render(<MasterForm />, document.getElementById('app'));
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
