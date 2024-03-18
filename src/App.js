26b75e2bc6daafde5f28d880f01fef5b94d29fea
Zach Watts <zachwatts@Zachs-MacBook-Pro.local>
2024-03-17 20:46:23 -0700

    Added SASS styling capability

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDom from 'react-dom';
-//import 'normalize.css/normalize.css';
-//import './styles/styles.scss';
+import './styles/styles.scss';
 import MasterForm from '../src/components/MasterForm';
 
 ReactDom.render(<MasterForm />, document.getElementById('root'));
